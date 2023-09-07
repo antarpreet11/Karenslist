87a06a7151503eac2fff3d556db0943e5d37dc80
Antarpreet Singh <84466072+antarpreet11@users.noreply.github.com>
2023-09-07 17:27:31 -0400

    fix: remaking frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Main from './pages/Main';
-import Profile from './pages/Profile';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {  
-
+function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Navigate to="/signup" />} />
-            <Route path="/main" element={<Main />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-        </Routes>
-    </BrowserRouter>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
